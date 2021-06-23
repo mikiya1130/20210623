106e9c0b1638ab6073bb31acd1d67e94ecb352b0
Morisaki Mikiya <git@morisakimikiya.com>
2021-06-23 14:55:47 +0900

    Add all files

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import "./styles.css";
+
+const canvas = document.querySelector("#draw-area");
+const context = canvas.getContext("2d");
+
+canvas.addEventListener("mousemove", (event) => {
+  draw(event.layerX, event.layerY);
+});
+canvas.addEventListener("touchmove", (event) => {
+  draw(event.layerX, event.layerY);
+});
+
+canvas.addEventListener("mousedown", () => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("mouseup", () => {
+  context.closePath();
+  isDrag = false;
+});
+canvas.addEventListener("touchstart", () => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("touchend", () => {
+  context.closePath();
+  isDrag = false;
+});
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+// 発展1.
+const color = document.getElementById("color");
+context.strokeStyle = color.value;
+color.addEventListener("change", () => {
+  context.strokeStyle = color.value;
+});
+
+// 発展2.
+const bgColor = document.getElementById("bgColor");
+canvas.style.background = bgColor.value;
+bgColor.addEventListener("change", () => {
+  canvas.style.background = bgColor.value;
+});
+
+let isDrag = false;
+function draw(x, y) {
+  if (!isDrag) {
+    return;
+  }
+
+  context.lineWidth = 5;
+  context.lineTo(x, y);
+  context.stroke();
+}
